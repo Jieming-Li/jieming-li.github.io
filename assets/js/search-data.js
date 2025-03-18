080e641e9e94b260ea0870c34ae527188bf54b56
Jieming-Li <153796484+Jieming-Li@users.noreply.github.com>
2025-03-18 08:37:42 +0000

    Deploying to gh-pages from @ Jieming-Li/jieming-li.github.io@33b410d06cbe885db58480b884111c53ad3c24de 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -429,11 +429,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-i-m-not-done-making-this-website-sorry-about-that-sweat-smile",
-          title: 'I’m not done making this website. Sorry about that! :sweat_smile:',
-          description: "",
-          section: "News",},{id: "news-i-am-offered-a-fully-funded-phd-position-in-computer-science-at-gergia-state-university",
+            },},{id: "news-i-am-offered-a-fully-funded-phd-position-in-computer-science-at-gergia-state-university",
           title: 'I am offered a fully funded PhD position in Computer Science at Gergia...',
+          description: "",
+          section: "News",},{id: "news-i-m-not-done-making-this-website-sorry-about-that-sweat-smile",
+          title: 'I’m not done making this website. Sorry about that! :sweat_smile:',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
