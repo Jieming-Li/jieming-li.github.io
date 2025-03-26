51cbfe16b8900e01145601538dc94dc1dd344eed
Jieming-Li <153796484+Jieming-Li@users.noreply.github.com>
2025-03-26 13:31:09 +0000

    Deploying to gh-pages from @ Jieming-Li/jieming-li.github.io@adda30b0072740edebe105d8ed28684c45e69ce6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,7 +23,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "books-the-godfather",
+        },{id: "post-know-them-flowers",
+      
+        title: "Know them flowers",
+      
+      description: "a simple collection",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/know-them-flowers/";
+        
+      },
+    },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
